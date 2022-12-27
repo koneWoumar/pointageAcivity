cd7ec52fd344a19758971ff64fb7ca1a76dc59f4
koneWoumar <kone.wolouho@gmail.com>
2022-12-27 02:01:22 +0100

    creation et inclusion des dependances de base du projet

---
+++ src/main/java/com/kwo/pointageActivity/PointageActivityApplication.java
@@ -0,0 +1,13 @@
+package com.kwo.pointageActivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointageActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointageActivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwo/pointageActivity/PointageActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwo.pointageActivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointageActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
